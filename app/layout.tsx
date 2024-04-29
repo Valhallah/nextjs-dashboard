6d593b945adc9cbe79e903be0a562f74d22bc73c
Valyn Hall <valynhall@gmail.com>
2024-04-29 14:41:24 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+"use` client";
+import '@/app/ui/global.css';
+import { inter } from '../app/ui/fonts/fonts'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <AppRouterCacheProvider>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      </AppRouterCacheProvider>
     </html>
   );
 }
